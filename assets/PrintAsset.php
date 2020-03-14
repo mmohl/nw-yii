6d1005bd3185783f6be2cafb5648ef82d035f59b
Maulana Mohammad Luthfy <maulana.mohammad.luthfy@gmail.com>
2020-03-14 11:53:01 +0700

    fix print pdf

---
+++ assets/PrintAsset.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class PrintAsset extends AppAsset
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [];
+    public $js = [];
+    public $depends = [
+        // 'yii\web\JqueryAsset',
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap\BootstrapPluginAsset',
+    ];
+}
---
+++ web/js/report_print.js
@@ -0,0 +1,3 @@
+$('#btn-print-report').on('click', function () {
+    window.open(`/report/print?month=1&year=2020`, '_blank')
+});
